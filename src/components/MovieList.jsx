7bc56ca8698f30eae56d1f8715469691e66accae
anjalidalal <anjalimaria2002@gmail.com>
2023-04-03 07:02:20 +0530

    Fixed some errors

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,34 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
-import CircularProgress from "@mui/material/CircularProgress";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+//import CircularProgress from "@mui/material/CircularProgress";
 
 const MovieList = () => {
   const [searchMovie, setSearchMovie] = useState();
   const [moviesArray, setMoviesArray] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  //const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    let name = localStorage.getItem("movie");
+    if (name) {
+      fetchMovies(name);
+    }
+  }, []);
 
   const handleSearchMovie = (e) => {
     setSearchMovie(e.target.value);
   };
 
-  const fetchMovie = (movieName) => {
-    setIsLoading(true);
+  const fetchMovies = (movieName) => {
     const url = `https://www.omdbapi.com/?s=${movieName}&apikey=f512a0ba`;
+    localStorage.setItem("movie", movieName);
     fetch(url)
       .then((response) => response.json())
       .then((result) => {
-        setMoviesArray(result.Search);
-        setIsLoading(false);
+        if (result.Error) {
+          setMoviesArray([]);
+          setError(true);
+        } else {
+          setMoviesArray(result.Search);
+          setError(false);
+        }
         console.log(result);
       })
       .catch((error) => {
         console.log(error);
-        setIsLoading(false);
-        setError(error.Error);
       });
   };
   console.log(error);
@@ -47,7 +58,7 @@ const MovieList = () => {
         <Button
           className="searchButton"
           variant="contained"
-          onClick={() => fetchMovie(searchMovie)}
+          onClick={() => fetchMovies(searchMovie)}
         >
           {" "}
           <SearchIcon fontSize="small" />
@@ -55,24 +66,23 @@ const MovieList = () => {
         </Button>
       </div>
       <p>Results for {searchMovie}</p>
-      {isLoading ? (
-        <div>
-          <CircularProgress />
-          Loading...
-        </div>
-      ) : (
-        <div className="moviesList">
-          {moviesArray?.map((el) => (
-            <Link to={`/${el.imdbID}`} key={el.imdbID}>
-              <div className="card">
-                <img src={el.Poster} className="poster" alt="" />
-                <h1 className="title">{el.Title}</h1>
-                <p className="releaseYear">Release - {el.Year}</p>
-              </div>
-            </Link>
-          ))}{" "}
-        </div>
-      )}
+
+      <div className="moviesList">
+        {moviesArray?.map((el) => (
+          <Link to={`/${el.imdbID}`} key={el.imdbID}>
+            <div className="card">
+              <img src={el.Poster} className="poster" alt="" />
+              <h1 className="title">{el.Title}</h1>
+              <p className="releaseYear">Release - {el.Year}</p>
+            </div>
+          </Link>
+        ))}{" "}
+      </div>
+      {error ? (
+        <h1 className="errorMsg">
+          Movie Not Found <SentimentVeryDissatisfiedIcon fontSize="large" />{" "}
+        </h1>
+      ) : null}
     </div>
   );
 };
